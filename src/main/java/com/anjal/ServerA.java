349b86668c83257352aa55fcda2e4aa6c21e3793
nzalmalla <medenimmalla@gmail.com>
2020-11-25 22:35:49 -0500

    nov2418

--- src/main/java/com/anjal/ServerA.java
+++ src/main/java/com/anjal/ServerA.java
@@ -10,35 +10,30 @@ import java.util.List;
 
 public class ServerA {
     static List<ClientHandler> ar = new ArrayList<>();
-
-
     static int i = 0;
 
-    public static void main(String[] args) throws IOException {
+    public ServerA {
         try (ServerSocket ss = new ServerSocket(9999)) {
             System.out.println("Server started at port " + 9999);
             Socket s;
             while (true) {
                 s = ss.accept();
-
                 System.out.println("Accepting new client: " + s);
-
                 DataInputStream din = new DataInputStream(s.getInputStream());
                 DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-
                 System.out.println("Created new client handler");
-
                 ClientHandler mtch = new ClientHandler(s, "client" + i, din, dout);
-
-                Thread t = new Thread(mtch);
-
-                System.out.println("Added to the Client list");
-
-                ar.add(mtch);
-
-                t.start();
-                i++;
             }
+        }
+
+        public static void main (String[]args) throws IOException {
+            ServerA newServer = new ServerA();
+            Thread t = new Thread(mtch);
+            System.out.println("Added to the Client list");
+            ar.add(mtch);
+            t.start();
+            i++;
+
         }
     }
 }
